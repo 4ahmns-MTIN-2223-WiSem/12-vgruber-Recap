85fcabe25560da1e8644f15cc93064951537cc8f
wiverite <wiverite@gmail.com>
2022-09-29 13:20:58 +0200

    ende der stunde

--- Assets/MyGame/Scripts/Unterricht/BoxTicTacToe.cs
+++ Assets/MyGame/Scripts/Unterricht/BoxTicTacToe.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum StateTTT
+{
+    white,
+    playerX,
+    playerO
+}
 
 public class BoxTicTacToe : MonoBehaviour
 {
+    StateTTT myState;
+    private Image myImage;
+    private ManagerTicTacToe myManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myState = StateTTT.white;
+        myImage = gameObject.GetComponent<Image>();
+        myManager = FindObjectOfType<ManagerTicTacToe>();
+
+        SetImageBasedOnState(myState);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PressMe()
     {
-        
+        if (myManager.player1)
+        {
+            myState = StateTTT.playerX;
+            SetImageBasedOnState(myState);
+
+        }
+        else
+        {
+            myState = StateTTT.playerO;
+            SetImageBasedOnState(myState);
+
+        }
+        Debug.Log(gameObject.name);
+
+        myManager.player1 = !myManager.player1;
+        myManager.ShowActivePlayer1(myManager.player1);
+    }
+
+    public void SetImageBasedOnState(StateTTT state)
+    {
+        switch (state)
+        {
+            case StateTTT.white:
+                myImage.sprite = myManager.imgWhite;
+                break;
+
+            case StateTTT.playerO:
+                myImage.sprite = myManager.imgO;
+                break;
+            case StateTTT.playerX:
+                myImage.sprite = myManager.imgX;
+                break;
+        }
     }
 }
--- Assets/MyGame/Scripts/Unterricht/ManagerTicTacToe.cs
+++ Assets/MyGame/Scripts/Unterricht/ManagerTicTacToe.cs
@@ -1,15 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManagerTicTacToe : MonoBehaviour
 {
 
     [SerializeField] private BoxTicTacToe[] arrayTT;
+    public Sprite imgWhite, imgX, imgO;
+    [SerializeField] private Image playerO, playerX;
+
+public bool player1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player1 = false;
+        ShowActivePlayer1(player1);
+    }
+
+    public void ShowActivePlayer1(bool isPlayer1)
+    {
+        if (isPlayer1)
+        {
+            playerO.color = Color.green;
+            playerX.color = Color.white;
+
+        }
+        else
+        {
+            playerX.color = Color.green;
+            playerO.color = Color.white;
+        }
+    }
+
+    public void ResetBoard()
+    {
+        for(int i = 0; i< arrayTT.Length; i++)
+        {
+            arrayTT[i].SetImageBasedOnState(StateTTT.white);
+        }
     }
 
     // Update is called once per frame
